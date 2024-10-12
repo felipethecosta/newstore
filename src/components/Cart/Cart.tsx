cb299a5640d3dc13da0dd141161733abbfe4303c
felipethecosta <felipethecosta@gmail.com>
2024-10-11 23:00:09 -0300

    feat: create a new component for cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
